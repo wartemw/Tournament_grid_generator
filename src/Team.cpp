ff529f58b61168a0fa598daaaf977d925290f3fe
romashka1337 <1337romashka1337@gmail.com>
2019-05-21 19:27:32 +0700

    [Team] Add variable title and setter, getter

--- src/Team.cpp
+++ src/Team.cpp
@@ -1,3 +1,11 @@
 #include "include/Team.h"
 
 Team::Team(string title) : _title(std::move(title)) {}
+
+const string &Team::getTitle() const {
+    return _title;
+}
+
+void Team::setTitle(const string &title) {
+    Team::_title = title;
+}
--- src/include/Team.h
+++ src/include/Team.h
@@ -5,10 +5,13 @@
 
 class Team {
 private:
-
+    string _title;
 public:
     Team(string title);
 
+    const string &getTitle() const;
+
+    void setTitle(const string &title);
 };
 
 #endif //TOURNAMENT_GRID_GENERATOR_TEAM_H
