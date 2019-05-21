a0f69336853ba2a3a8d9c6f24d37821a0be3d9e8
romashka1337 <1337romashka1337@gmail.com>
2019-05-21 19:09:11 +0700

    [Stdaf] Include default libraries

---
+++ src/include/Stdaf.h
@@ -0,0 +1,15 @@
+#ifndef TOURNAMENT_GRID_GENERATOR_STDAF_H
+#define TOURNAMENT_GRID_GENERATOR_STDAF_H
+
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <limits>
+#include <string>
+#include <vector>
+#include <math.h>
+
+
+using namespace std;
+
+#endif //TOURNAMENT_GRID_GENERATOR_STDAF_H
