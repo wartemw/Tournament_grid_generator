785a96871a2042d89138699f66fbc4f15431066b
romashka1337 <1337romashka1337@gmail.com>
2019-05-22 17:50:00 +0700

    [GridManager] Include Stdaf library

--- src/include/GridManager.h
+++ src/include/GridManager.h
@@ -1,8 +1,11 @@
 #ifndef TOURNAMENT_GRID_GENERATOR_GRIDMANAGER_H
 #define TOURNAMENT_GRID_GENERATOR_GRIDMANAGER_H
 
+#include "include/Stdaf.h"
+
 class GridManager {
 private:
 public:
 };
+
 #endif //TOURNAMENT_GRID_GENERATOR_GRIDMANAGER_H
