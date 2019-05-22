7c13da0eba7aeed903920c4070837fa1e4e74002
wartemw <wartemw@gmail.com>
2019-05-22 17:36:42 +0700

    [RoundGraph] Add variable root, pair, next, prefLeft, prefRight

--- src/include/RoundGraph.h
+++ src/include/RoundGraph.h
@@ -2,9 +2,16 @@
 #define TOURNAMENT_GRID_GENERATOR_ROUNDGRAPH_H
 
 #include "Stdaf.h"
+#include "Round.h"
+#include "PairGraph.h"
 
 class RoundGraph {
 private:
+    Round *root = nullptr;
+    PairGraph *_pair = nullptr;
+    RoundGraph *_next = nullptr;
+    RoundGraph *_prefLeft = nullptr;
+    RoundGraph *_prefRight = nullptr;
 public:
 };
 
