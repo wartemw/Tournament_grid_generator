176c20dce8f7fb3fe0b867514f5a991dbb374c3a
romashka1337 <1337romashka1337@gmail.com>
2019-05-22 17:52:39 +0700

    [GridManager] Add validate teamList and method addTeam

--- src/GridManager.cpp
+++ src/GridManager.cpp
@@ -1,5 +1,9 @@
 #include "GridManager.h"
 
+void GridManager::addTeam(Team *team) {
+    _teamList.push_back(team);
+}
+
 void GridManager::setType(int type) {
     _type = type;
 }
--- src/include/GridManager.h
+++ src/include/GridManager.h
@@ -2,11 +2,14 @@
 #define TOURNAMENT_GRID_GENERATOR_GRIDMANAGER_H
 
 #include "include/Stdaf.h"
+#include "Round.h"
 
 class GridManager {
 private:
     int _type;
+    vector<Team*> _teamList;
 public:
+    void addTeam(Team *team);
     void setType(int type);
 };
 
