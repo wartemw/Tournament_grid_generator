b8f22f0a23df1f1781553ae4e1ed19d085679faa
wartemw <wartemw@gmail.com>
2019-05-22 16:17:43 +0700

    [PairGraph] Add variable pair and teamList

--- src/include/PairGraph.h
+++ src/include/PairGraph.h
@@ -2,9 +2,13 @@
 #define TOURNAMENT_GRID_GENERATOR_PAIRGRAPH_H
 
 #include "Stdaf.h"
+#include "Pair.h"
+#include "TeamGraph.h"
 
 class PairGraph {
 private:
+    Pair* pair;
+    vector<TeamGraph *> _teamList;
 public:
 };
 
