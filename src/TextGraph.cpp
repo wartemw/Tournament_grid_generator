1ab08a0409d62f8374b27df7943dcb8883b02ef9
wartemw <wartemw@gmail.com>
2019-05-22 15:46:39 +0700

    [TextGraph] Add variable text,x,y and getters and setters

--- src/TextGraph.cpp
+++ src/TextGraph.cpp
@@ -1,1 +1,25 @@
 #include "TextGraph.h"
+
+string TextGraph::getText() {
+    return text;
+}
+
+void TextGraph::setText(string text) {
+    TextGraph::text = text;
+}
+
+int TextGraph::getX() const {
+    return x;
+}
+
+void TextGraph::setX(int x) {
+    TextGraph::x = x;
+}
+
+int TextGraph::getY() const {
+    return y;
+}
+
+void TextGraph::setY(int y) {
+    TextGraph::y = y;
+}
--- src/include/TextGraph.h
+++ src/include/TextGraph.h
@@ -5,7 +5,21 @@
 
 class TextGraph {
 private:
+    string text;
+    int x;
+    int y;
 public:
+    string getText();
+
+    void setText(string text);
+
+    int getX() const;
+
+    void setX(int x);
+
+    int getY() const;
+
+    void setY(int y);
 };
 
 #endif //TOURNAMENT_GRID_GENERATOR_TEXTGRAPH_H
