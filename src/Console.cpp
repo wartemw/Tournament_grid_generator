19e92a163106374ce3b7fbdadb85ae5986a226a7
HataBeach <nikolya.markov.1999@mail.ru>
2019-05-29 17:59:32 +0700

    [Console] Realisation method menu

--- src/Console.cpp
+++ src/Console.cpp
@@ -1,7 +1,23 @@
 #include "Console.h"
 
 void Console::menu() {
+    int num = 0;
+    while (true) {
+        clearScreen();
+        cout << "Меню: " << endl;
+        cout << "\t" << "1) " << "создать сетку," << endl;
+        cout << "\t" << "2) " << "информация," << endl;
+        cout << "\t" << "3) " << "выход." << endl;
+        cout << "Введите номер для перехода: ";
 
+        while (!(cin >> num)) {
+            cin.clear();
+            cin.get();
+            break;
+        }
+
+        if (moveToItem(num)) break;
+    }
 }
 
 bool Console::moveToItem(int num) {
