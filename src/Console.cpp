4bfd966b5130c693c5ee44a3c7610849d5227d11
HataBeach <nikolya.markov.1999@mail.ru>
2019-05-29 17:52:56 +0700

    [Console] Add methods menu, moveToItem, createGrid, info

--- src/Console.cpp
+++ src/Console.cpp
@@ -1,1 +1,17 @@
 #include "Console.h"
+
+void Console::menu() {
+
+}
+
+bool Console::moveToItem(int num) {
+    return false;
+}
+
+void Console::createGrid() {
+
+}
+
+void Console::info() {
+
+}
--- src/include/Console.h
+++ src/include/Console.h
@@ -1,9 +1,20 @@
 #ifndef TOURNAMENT_GRID_GENERATOR_CONSOLE_H
 #define TOURNAMENT_GRID_GENERATOR_CONSOLE_H
 
+#include "include/Stdaf.h"
+#include "GridManager.h"
+#include "Team.h"
+#include <cstdlib>
 
 class Console {
+private:
+    void menu();
 
+    bool moveToItem(int num);
+
+    void createGrid();
+
+    void info();
 };
 
 
