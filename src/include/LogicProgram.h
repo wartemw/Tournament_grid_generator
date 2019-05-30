810382ae283448189c9a1b4265267e39006efc74
wartemw <wartemw@gmail.com>
2019-05-30 09:40:19 +0700

    [LogicProgram] Add virtual function set

--- src/include/LogicProgram.h
+++ src/include/LogicProgram.h
@@ -2,11 +2,15 @@
 #define TOURNAMENT_GRID_GENERATOR_LOGICPROGRAM_H
 
 #include "Windows.h"
+#include "Round.h"
 
 class Windows;
+class Round;
 
 class LogicProgram {
 public:
+    virtual void set(Round *round) = 0;
+
     virtual void prepare(Windows *windows) = 0;
 
     virtual void render(Windows *windows) = 0;
